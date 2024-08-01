25a06f48566509d702b00f560dc42c5b89534362
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-08-01 21:41:15 +0300

    fix

--- src/common/interfaces/abstract_content_finder.py
+++ src/common/interfaces/abstract_content_finder.py
@@ -6,22 +6,6 @@ from src.common.dto.content import ContentDTO
 
 class AbstractContentFinder(ABC):
 
-    @property
     @abstractmethod
-    def _host(self) -> str:
-        """
-        This is a class attribute.
-
-        Usage:
-
-            class MyContentFinder(AbstractContentFinder):
-                _host = 'your_host'
-
-                ...
-        """
-
-        raise NotImplementedError
-
-    @abstractmethod
-    def find_content(self, headers: dict, cookies: Optional[dict] = None) -> list[ContentDTO]:
+    def find_content(self, url: str, headers: dict, cookies: Optional[dict] = None) -> list[ContentDTO]:
         raise NotImplementedError
